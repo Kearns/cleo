6be430d55df6eb554fd0c807f01eac09726f54f0
PK <ptkearns@outlook.com>
2021-07-29 20:32:00 -0500

    feat: added simple router poc

--- packages/cleo-client/src/index.ts
+++ packages/cleo-client/src/index.ts
@@ -1,12 +1,25 @@
 
 import ReactWebComponent from './loaders/ReactLoader';
-import {SampleComponent} from './samples/index';
+import { SampleComponent, SampleComponentTwo } from './samples/index';
+import { Router } from './router/index';
+import './elements/NavLink'
+
 const domElementContainer = document.getElementById('cleo-container');
 
 new ReactWebComponent(
-  'x-demo-react-app', 
+  'x-demo-react-app-one',
   SampleComponent
 );
 
+new ReactWebComponent(
+  'x-demo-react-app-two',
+  SampleComponentTwo
+);
 
-domElementContainer.appendChild(document.createElement('x-demo-react-app'))
+
+const router = new Router({
+  '/1': '<x-demo-react-app-one>',
+  '/2': '<x-demo-react-app-two>'
+}, domElementContainer);
+
+(window as any).router = router;
--- packages/cleo-client/src/samples/index.tsx
+++ packages/cleo-client/src/samples/index.tsx
@@ -1,3 +1,11 @@
 import React from 'react';
 
-export const SampleComponent = () => <div>WOKRING</div>
+export const SampleComponent = () => {
+  const [number, setNumber] = React.useState(Math.random())
+  return <div>SAMPLE 1: {number}</div>
+};
+
+export const SampleComponentTwo = () => {
+  const [number, setNumber] = React.useState(Math.random())
+  return <div>SAMPLE 2: {number}</div>
+};
